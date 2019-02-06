3e051244fea93dcfb343220b1729e84be0cc7154
Celia Marshall <celia.m@gmail.com>
2019-02-06 09:00:17 -0800

    update migrations

--- db/migrations/20190205131852_create_runs_table.js
+++ db/migrations/20190205131852_create_runs_table.js
@@ -2,13 +2,14 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('runs', (table) => {
     table.increments();
-    table.string('description');
     table.string('day').notNullable();
     table.time('time').notNullable();
     table.text('location').notNullable();
     table.string('run_type').notNullable();
-    table.text('pace');
-    table.string('terrain');
+    table.string('terrain').notNullable();
+    table.string('pace');
+    table.string('distance')
+    table.string('description');
   })
 };
 
